8f02e867b3dd5a158325464af3ac7bf11d5d7b54
Dave Workman <davidworkman9@gmail.com>
2014-12-16 17:15:10 -0500

    added bucket cache to speed up bucket retrieval, added deleteObject, headObject getObject, and deleteBucket operations and tests for each

--- main.js
+++ main.js
@@ -2,6 +2,129 @@ var express = require('express');
 var app = express();
 var bucketManager = require('./lib/buckets.js');
 
+
+/*********************
+ * Object Operations *
+ * *******************
+ */
+
+/**
+ * deleteObject
+ * ------------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteObject-property
+ */
+app.delete('/*/*', function (req, res) {
+    var info = parseUrl(req.url);
+    res.set('Content-Type', 'text/xml');
+    if (!info.bucket) {
+        return res.status(403).send(formulateError({
+            code: 'Access Denied',
+            message: 'Bucket name required'
+        })).end();
+    }
+    bucketManager.onReady(function () {
+        bucketManager.getBucket(info.bucket, function (err, bucket) {
+            if (err)
+                return res.status(403).send(formulateError({
+                    code: 'Access Denied',
+                    message: err.toString()
+                })).end();
+            bucket.deleteFile(info.key, function (err) {
+                if (err)
+                    return res.status(403).send(formulateError({
+                        code: 'Access Denied',
+                        message: err.toString()
+                    })).end();
+
+                res.status(200).end();
+            });
+        });
+    });
+});
+
+/**
+ * headObject
+ * ----------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#headObject-property
+ */
+app.head('/*/*', function (req, res) {
+    var info = parseUrl(req.url);
+    res.set('Content-Type', 'text/xml');
+    if (!info.bucket) {
+        return res.status(403).send(formulateError({
+            code: 'Access Denied',
+            message: 'Bucket name required'
+        })).end();
+    }
+    bucketManager.onReady(function () {
+        bucketManager.getBucket(info.bucket, function (err, bucket) {
+            if (err)
+                return res.status(403).send(formulateError({
+                    code: 'Access Denied',
+                    message: err.toString()
+                })).end();
+
+            bucket.getMD5Hash(info.key, function (err, md5) {
+                if (err)
+                    return res.status(403).send(formulateError({
+                        code: 'Access Denied',
+                        message: err.toString()
+                    })).end();
+                res.setHeader('ETag', '"' + md5 + '"');
+                res.status(200).end();
+            });
+        });
+    });
+});
+
+/**
+ *  getObject
+ *  ---------
+ *  http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
+ */
+app.get('/*/*', function (req, res) {
+    var info = parseUrl(req.url);
+    res.set('Content-Type', 'text/xml');
+    if (!info.bucket) {
+        return res.status(403).send(formulateError({
+            code: 'Access Denied',
+            message: 'Bucket name required'
+        })).end();
+    }
+    bucketManager.onReady(function () {
+        bucketManager.getBucket(info.bucket, function (err, bucket) {
+            if (err)
+                return res.status(403).send(formulateError({
+                    code: 'Access Denied',
+                    message: err.toString()
+                })).end();
+
+            bucket.getFile(info.key, function (err, stream, stat) {
+                if (err) {
+                    var code = 'Error';
+                    if (err.toString() === 'Error: 404: File not found.') {
+                        code = 'NoSuchKey';
+                        err = 'The specified key does not exist.';
+                    }
+                    return res.status(404).send(formulateError({
+                        code: code,
+                        message: err.toString()
+                    })).end();
+                }
+
+                res.setHeader('content-type', stat.type);
+                res.status(200);
+                stream.pipe(res);
+            });
+        });
+    });
+});
+
+/**
+ * putObject
+ * ---------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+ */
 app.put('/*/*', function (req, res) {
     var info = parseUrl(req.url);
     res.set('Content-Type', 'text/xml');
@@ -33,6 +156,50 @@ app.put('/*/*', function (req, res) {
     });
 });
 
+/*********************
+ * Bucket Operations *
+ * *******************
+ */
+
+/**
+ * listBuckets
+ * -----------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listBuckets-property
+ */
+
+// TODO: Implement
+
+/**
+ * deleteBucket
+ * ------------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteBucket-property
+ */
+app.delete('/*', function (req, res) {
+    var info = parseUrl(req.url);
+    res.set('Content-Type', 'text/xml');
+    if (!info.bucket) {
+        return res.status(403).send(formulateError({
+            code: 'Access Denied',
+            message: 'Bucket name required'
+        })).end();
+    }
+    bucketManager.onReady(function () {
+        bucketManager.deleteBucket(info.bucket, function (err) {
+            if (err)
+                return res.status(500).send(formulateError({
+                    code: 'Error',
+                    message: err.toString()
+                })).end();
+            res.status(200).end();
+        });
+    });
+});
+
+/**
+ * createBucket
+ * ------------
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#createBucket-property
+ */
 app.put('/*', function (req, res) {
     var info = parseUrl(req.url);
     res.set('Content-Type', 'text/xml');
@@ -56,18 +223,8 @@ app.put('/*', function (req, res) {
         });
     });
 });
-//
-//app.get('/', function(req, res) {
-//    res.send("Hello Cybertron!");
-//    console.log(req);
-//});
-//
-//app.get('/insecticons.json', function(req, res) {
-//    res.writeHead(200, { 'Content-Type': 'application/json' });
-//    res.write(JSON.stringify({insecticons : ["Shrapnel","Bombshell", "Kickback"]}));
-//    res.end();
-//});
 
+// Webs server setup
 var port = process.env.PORT || 7000;
 app.listen(port);
 
--- test/s3Tests.js
+++ test/s3Tests.js
@@ -1,6 +1,7 @@
 var chai = require('chai'),
     fs = require('fs'),
     expect = chai.expect,
+    _ = require('lodash'),
     proxy = require('proxy-agent'),
     server,
     cleanup = require('../testCleanup.js'),
@@ -9,6 +10,10 @@ var chai = require('chai'),
 describe('test S3 compatibility', function () {
     var s3;
     var bucketName = 's4Testing';
+    var obj = {
+        Bucket: bucketName,
+        Key: 'thisIsATestKey'
+    };
     before(function (done) {
         cleanup(function () {
             server = require('../main.js');
@@ -16,6 +21,12 @@ describe('test S3 compatibility', function () {
                 sslEnabled: false,
                 httpOptions: { agent: proxy('http://localhost:7000') }
             });
+
+            AWS.config.credentials = {
+                "accessKeyId": "key",
+                "secretAccessKey": "secret",
+                "region": "us-east-1"
+            };
 
             s3 = new AWS.S3();
             done();
@@ -33,13 +44,59 @@ describe('test S3 compatibility', function () {
     });
 
     it('puts a file in a bucket', function (done) {
-        s3.putObject({
-            Bucket: bucketName,
-            Key: 'thisIsATestKey',
+        s3.putObject(_.extend({
             Body: fs.createReadStream('testFile.txt')
-        }, function (err, res) {
+        }, obj), function (err, res) {
             expect(err).to.not.exist;
             expect(res.ETag).to.be.equal('"954c779488b31fdbe52e364fa0a71045"');
+            done();
+        });
+    });
+
+    it('retrieve a file', function (done) {
+        s3.getObject(obj, function (err, res) {
+            expect(err).to.not.exist;
+            expect(res.Body.toString()).to.be.equal('this is a test file, nothing else.');
+            done();
+        });
+    });
+
+    it("verifies the file's md5 hash", function (done) {
+        s3.headObject(obj, function (err, res) {
+            expect(err).to.not.exist;
+            expect(res.ETag).to.be.equal('"954c779488b31fdbe52e364fa0a71045"');
+            done();
+        });
+    });
+
+    it('deletes a file in a bucket', function (done) {
+        s3.deleteObject(obj, function (err, res) {
+            expect(err).to.not.exist;
+            expect(res).to.be.empty;
+            done();
+        });
+    });
+
+    it('verifies file is gone', function (done) {
+        s3.getObject(obj, function (err) {
+            expect(err).to.exist;
+            expect(err.toString()).to.be.equal('NoSuchKey: The specified key does not exist.');
+            done();
+        });
+    });
+
+    it('deletes a bucket', function (done) {
+        s3.deleteBucket({ Bucket: bucketName }, function (err, res) {
+            expect(err).to.not.exist;
+            expect(res).to.be.empty;
+            done();
+        });
+    });
+
+    it('verifies bucket has been deleted', function (done) {
+        s3.listBuckets(function (err, res) {
+            expect(err).to.not.exist;
+            expect(_.pluck(res.Buckets, 'Name')).to.not.include(bucketName);
             done();
         });
     });
